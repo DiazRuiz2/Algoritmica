66f7fae0d3361e679d91f4d3185ee8559f76c952
Gonzalo <jukkeas@gmail.com>
2023-04-11 18:34:37 +0200

    Cambios realizados

--- DyV/dyv.cpp
+++ DyV/dyv.cpp
@@ -4,6 +4,7 @@
 #include<time.h>
 #include<map>
 #include<chrono>
+#include<fstream>
 
 
 using namespace std;
@@ -30,12 +31,12 @@ bool Sort_Second_Coordinate(const pair<Point_Value,int> &x, const pair<Point_Val
 }
 
 bool Sort_Struct(const Point_Value &x, const Point_Value &y){
-	if(x.point.eje_x==y.point.eje_x) return (x.point.eje_y<x.point.eje_x);
+	if(x.point.eje_x==y.point.eje_x) return (x.point.eje_y<x.point.eje_y);
 	else return x.point.eje_x<y.point.eje_x;
 }
 
 
-void DivideYVenceras(vector<Point_Value> &valores, vector<vector<Point2D>> &dominados){
+void DivideYVenceras(vector<Point_Value> &valores){
 	
 	//Caso base que devolvemos.
 	if(valores.size()==1) return;
@@ -44,11 +45,9 @@ void DivideYVenceras(vector<Point_Value> &valores, vector<vector<Point2D>> &domi
 	vector<Point_Value> A(valores.begin(),(valores.begin()+valores.size()/2));
 	vector<Point_Value> B((valores.begin()+valores.size()/2),valores.end());
 	
-	vector<vector<Point2D>> dom_A(dominados.begin(),(dominados.begin()+dominados.size()/2));
-	vector<vector<Point2D>> dom_B((dominados.begin()+dominados.size()/2),dominados.end());
 
-	DivideYVenceras(A,dom_A);
-	DivideYVenceras(B,dom_B);
+	DivideYVenceras(A);
+	DivideYVenceras(B);
 	//cout << dominados.size() << endl;
 	for(int x=0;x<A.size();x++){
 		valores[x]=A[x];
@@ -57,13 +56,6 @@ void DivideYVenceras(vector<Point_Value> &valores, vector<vector<Point2D>> &domi
 		valores[A.size()+x]=B[x];
 	}
 	
-	for(int x=0; x<dom_A.size();x++){
-		dominados[x]=(dom_A[x]);
-	}
-	
-	for(int x=0; x<dom_B.size();x++){
-		dominados[x]=(dom_B[x]);
-	}
 	
 	/*Vector que almacena:
 		first--> Punto y valor dominado en struct Point_Value
@@ -93,7 +85,7 @@ void DivideYVenceras(vector<Point_Value> &valores, vector<vector<Point2D>> &domi
 	
 	map<int,int> valores_iguales;	
 	int contador = 0;
-	vector<Point2D> val_dominados;
+	
 	
 	for(int x=0;x<lista.size();x++){//O(n)
 		
@@ -105,7 +97,7 @@ void DivideYVenceras(vector<Point_Value> &valores, vector<vector<Point2D>> &domi
 			//primera variable del valor del punto, algo que tendremos que 
 			//tener en cuenta a la hora de sumar el número de puntos dominados.
 			valores_iguales[lista[x].first.point.eje_x]+=1;
-			val_dominados.push_back(lista[x].first.point);
+			
 			
 		} 
 		//Si es un punto del vector B y no es el primero.
@@ -129,12 +121,7 @@ void DivideYVenceras(vector<Point_Value> &valores, vector<vector<Point2D>> &domi
 				y número de eje_x(valores_iguales...).
 			*/	 
 			valores[(valores.size()/2)+lista[x].second].value += contador_aux-valores_iguales[lista[x].first.point.eje_x];
-			for(int y=0;y<val_dominados.size();y++){
-				if(lista[x].first.point.eje_x!=val_dominados[y].eje_x && (lista[x].first.point.eje_y!=val_dominados[y].eje_y)){
-					//cout << val_dominados[y].eje_x << " " << val_dominados[y].eje_y << " " << lista[x].first.point.eje_x << " " <<lista[x].first.point.eje_y <<  endl;
-					dominados[dominados.size()/2+lista[x].second].push_back(val_dominados[y]);
-				}
-			}
+			
 		}
 	}
 	
@@ -148,30 +135,37 @@ int main(int argc,char ** argv){
 	
 	
 	
+	vector<Point_Value> valores;
+	ifstream leer(argv[1]);
 	
-	int n = 100000;
-	vector<vector<Point2D>> dominados(n);
-	vector<Point_Value> valores(n);
-	srand(time(0));
-	for (int x=0; x<n;x++){
-		int y = rand() %n;
-		int z = rand() %n;
-		Point2D val = {y,z};
-		valores[x] = {val,0};
+	if(leer.is_open()){
+		cout << "leyendo" << endl;
+		int x;
+		int y;
+		leer >> x;
+		while(x!=-1){
+			leer>>y;
+			Point2D val ={x,y};
+			Point_Value val2= {val,0};
+			valores.push_back(val2);
+			leer >> x;
+			
+		}
 		
 	}
-
+	leer.close();
 	
+	cout << valores.size() << endl;
 	sort(valores.begin(),valores.end(),Sort_Struct); //Ordenamos primera coordenada.
-	
+	cout << 1 << endl;
 	std::chrono::high_resolution_clock::time_point t_antes2, t_despues2;
 	t_antes2 = std::chrono::high_resolution_clock::now();
-  	DivideYVenceras(valores,dominados);
+  	DivideYVenceras(valores);
 	t_despues2 = std::chrono::high_resolution_clock::now();
   	
 	auto transcurrido2 = std::chrono::duration_cast<std::
 						chrono::duration<double>>(t_despues2 - t_antes2);
-  	cout << "Número de datos: " << n << "  Tiempo transcurrido: "
+  	cout << "Número de datos: " << valores.size() << "  Tiempo transcurrido: "
 	  	 << transcurrido2.count() << endl;
 	
 	
